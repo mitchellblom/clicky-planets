f4f6132ae4159153b13daf2cff6fd0c3d3cf4ca5
Mitchell Blom <mitchellblom@gmail.com>
2017-02-21 21:35:05 -0600

    classnotes complete, added clear button and toggle

--- scripts.js
+++ scripts.js
@@ -1,52 +1,53 @@
 // calls a function, and adds any string i want to the div, using innerHTML
 
 var showButton = document.getElementById("showPlanets");
-// var clearButton = document.getElementById("clearAll");
+var clearButton = document.getElementById("clearAll");
 var planetHolderDiv = document.getElementById("planetHolder");
 
 var planets = [
 {
 	name: "Mercury",
-	url: ""
+	url: "https://www.nasa.gov/sites/default/files/mercury_1.jpg"
 },
 {
 	name: "Venus",
-	url: ""
+	url: "https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg"
 },
 {
 	name: "Earth",
-	url: ""
+	url: "https://www.nasa.gov/centers/goddard/images/content/638831main_globe_east_2048.jpg"
 },
 {
 	name: "Mars",
-	url: ""
+	url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Mars_23_aug_2003_hubble.jpg/275px-Mars_23_aug_2003_hubble.jpg"
 },
 {
 	name: "Jupiter",
-	url: ""
+	url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Hubble_Captures_Vivid_Auroras_in_Jupiter's_Atmosphere.jpg/220px-Hubble_Captures_Vivid_Auroras_in_Jupiter's_Atmosphere.jpg"
 },
 {
 	name: "Saturn",
-	url: ""
+	url: "http://nssdc.gsfc.nasa.gov/image/planetary/saturn/saturn.jpg"
 },
 {
 	name: "Uranus",
-	url: ""
+	url: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg"
 },
 {
 	name: "Neptune",
-	url: ""
+	url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/260px-Neptune_Full.jpg"
 }];
 
 showButton.addEventListener("click", populatePage);
-// clearButton.addEventListener("click", cleanUp);
+clearButton.addEventListener("click", cleanUp);
 
 function populatePage() {
 	planetHolderDiv.innerHTML = "";
 	for (var i = 0; i < planets.length; i++) {
 		var newPlanet="";
 		newPlanet += `<div class="planetBox" id="planetBox-${i}">`
-		newPlanet += `<div class="planetName"> ${planets[i].name} </div>`;
+		newPlanet += `<div class="planetName hidden"> ${planets[i].name} </div>`;
+		newPlanet += `<img class="planetImage" src="${planets[i].url}"`
 		newPlanet += `</div>`
 		planetHolderDiv.innerHTML += newPlanet;
 	}
@@ -55,3 +56,20 @@ function populatePage() {
 function cleanUp() {
 	planetHolderDiv.innerHTML = "";
 };
+
+function showPlanetName(event){
+	if(event.target.className === 'planetImage'){
+	console.log("event worked", event);
+	// console.log("unique ID", event.target.parentNode.id)
+	console.log("text?", event.target.previousSibling);
+	event.target.previousSibling.classList.toggle('hidden');
+	}
+}
+
+showButton.addEventListener("mouseenter", populatePage);
+// showButton.addEventListener("mouseleave", cleanUp);
+
+document.body.addEventListener("click", showPlanetName);
+
+
+
